d87bc9abd1d07919aacee80524fd0024e266c44d
Eduard Komleu <eduard_komleu@epam.com>
2018-03-29 14:46:00 +0300

    refactoring

--- src/test/java/com/github/testcases/runners/SmokeTestRunner.java
+++ src/test/java/com/github/testcases/runners/SmokeTestRunner.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 @CucumberOptions(
        // plugin = {"html:target/cucumber-report/smoketest", "json:target/cucumber.json"},
         features = "src/test/resources/features",
-        glue = "src/test/resources/com/github/testcases/stepDefinitions",
+        glue = "com.github.testcases/stepDefinitions",
         tags = {"@smokeTest"}
 )
 @Test
